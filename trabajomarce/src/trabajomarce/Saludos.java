3dc36e3104f06d9b8510853ff8ab4a8f25cb6d58
erizcap <eriztrabajo@gmail.com>
2024-04-23 21:08:52 +0200

    Mensaje de Eriz

--- trabajomarce/src/trabajomarce/Saludos.java
+++ trabajomarce/src/trabajomarce/Saludos.java
@@ -12,8 +12,8 @@ public class Saludos {
 		
 		System.out.println("Bosco dice Hola");
 		
-		System.out.println("Bosco dice: la gallina vino antes que el huevo");
-
+		System.out.println("eriz dice: el huevo vino antes que la gallina");
+		
 	}
 
 }
